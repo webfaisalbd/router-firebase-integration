413e58916e6b204c838eca7fe61a89fbe62bb426
webfaisalbd <web.faisal.bd@gmail.com>
2022-04-15 17:52:59 +0600

    RequireAuth added(login first,then redirect your destination)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import Register from './components/Register/Register';
 import Products from './components/Products/Products';
+import Orders from './components/Orders/Orders';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -13,7 +15,14 @@ function App() {
         <Header></Header>
         <Routes>
             <Route path='/' element={<Home></Home>}></Route>
-            <Route path='/products' element={<Products></Products>}></Route>
+            <Route path='/products' element={
+              <RequireAuth>
+                <Products></Products>
+              </RequireAuth>
+            }></Route>
+            <Route path='/orders' element={
+              <RequireAuth><Orders></Orders></RequireAuth>
+            }></Route>
             <Route path='/login' element={<Login></Login>}></Route>
             <Route path='/register' element={<Register></Register>}></Route>
         </Routes>
